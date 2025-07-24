26525c82893d37b75f7a41e3efa6d1e73ece4ae7
Matt Mullen <mhmullen@icloud.com>
2025-07-24 16:55:52 -0700

    added tsconfig to FINALLY get compilation

---
+++ clean-sarahs-pilates-client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="text-center p-10 text-2xl text-orange-600">
+      <h1>Welcome to Sarah's Pilates</h1>
+      <p>This is a clean, working React + Tailwind setup!</p>
+    </div>
+  );
+};
+
+export default App;
