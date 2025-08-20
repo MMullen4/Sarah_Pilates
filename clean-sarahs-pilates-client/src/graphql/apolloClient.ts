572fc1ce83bbaf4d8e0eed536158101ece7292c6
Matt Mullen <mhmullen@icloud.com>
2025-08-20 12:13:47 -0700

    rev apolloClient

--- clean-sarahs-pilates-client/src/graphql/apolloClient.ts
+++ clean-sarahs-pilates-client/src/graphql/apolloClient.ts
@@ -1,10 +1,9 @@
-// client/src/apolloClient.ts
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const isDev = import.meta.env.DEV;
 const uri = isDev
   ? import.meta.env.VITE_GRAPHQL_URI || "http://localhost:3000/graphql"
-  : `${window.location.origin}/graphql`; // <-- forces prod to your Railway origin
+  : `${window.location.origin}/graphql`;
 
 export default new ApolloClient({
   uri,
